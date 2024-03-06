8cc93744e444444564877bae7a20c654306d2003
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-03-06 15:48:59 +0530

    First program

---
+++ JSpiders/Java_Pogramming_part1/MyName.java
@@ -0,0 +1,11 @@
+class  MyName
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Name: Ayush Prajapati");
+		System.out.println("Age: 23");
+		System.out.println("Phone_number: 1234567890");
+		System.out.println("E-mail: ayush.prajapati@gmail.com");
+		System.out.println("Trainee at JSpiders");
+	}
+}
