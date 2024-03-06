1704f6ce999fa64fddaa3d321d6c59e0a16cb21a
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-03-06 23:04:27 +0530

    Arithmetic operator

---
+++ JSpiders/Java_Pogramming_part1/ArithmeticOperator.java
@@ -0,0 +1,14 @@
+class ArithmeticOperator 
+{
+	public static void main(String[] args) 
+	{
+
+		System.out.println(5+6);
+		System.out.println(12-5);
+		System.out.println(5*6);
+		System.out.println(15/2);
+		System.out.println(15%6);
+		System.out.println(15.0/4);
+		System.out.println(15.0%3);
+	}
+}
