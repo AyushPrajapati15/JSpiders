b5a8762a5f10c6ed237f91ca4b8f7d5230001c10
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-03-07 22:40:13 +0530

    Voting eligibility

---
+++ JSpiders/Java_Pogramming_part1/VotingEligibility.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class VotingEligibility 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scan= new Scanner(System.in);
+		System.out.println("Enter your age");
+		int age=scan.nextInt();
+		if (age>=18)
+		{
+			System.out.println("You are eligible for voting");
+		}
+		else{
+			System.out.println("You are not eligible for voting");
+		}
+	}
+}
