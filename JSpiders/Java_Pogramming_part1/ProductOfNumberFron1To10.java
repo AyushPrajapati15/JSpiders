f8a850ddc2e0f73fa20437ca5da4bfb9ad923209
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-03-11 22:40:17 +0530

    Product of numbers from 1-10

---
+++ JSpiders/Java_Pogramming_part1/ProductOfNumberFron1To10.java
@@ -0,0 +1,12 @@
+class ProductOfNumberFron1To10 
+{
+	public static void main(String[] args) 
+	{
+		int prod=1;
+		for (int i=1;i<=10 ;i++ )
+		{
+			prod=prod*i;
+		}
+		System.out.println(prod);
+	}
+}
