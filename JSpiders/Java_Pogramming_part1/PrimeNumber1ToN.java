efb86ed001a150a28587778ed6083fedec57e307
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-03-19 22:58:43 +0530

    Prime Number 1 To N

---
+++ JSpiders/Java_Pogramming_part1/PrimeNumber1ToN.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class PrimeNumber1ToN
+{
+	public static void main(String[] args) 
+	{
+		/*Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of n");
+		int n=sc.nextInt();// to take input from user
+		for (int x=1;x<=n ;x++ ) //to check the prime number between 1 to n
+		{
+			int count=0;
+			for (int i=1;i<=x ;i++ )
+			{
+				if (x%i==0)//check the factors of x
+				{
+					count++;
+				}
+			}
+			if (count==2)//count is 2 then print x 
+			{
+				System.out.println(x);
+			}
+			18,446,744,073,709,551,616
+		}*/
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+	}
+}
