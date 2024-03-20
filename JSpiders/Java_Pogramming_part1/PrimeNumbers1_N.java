27f71fbd9e9ef6f56622ce0caa8a0749dfa2bb46
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-03-20 23:33:54 +0530

    Prime Numbers 1 to N

---
+++ JSpiders/Java_Pogramming_part1/PrimeNumbers1_N.java
@@ -0,0 +1,26 @@
+//WAPTP 1 to N Prime Number
+import java.util.Scanner;
+class PrimeNumbers1_N
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter n value:");
+		int n=sc.nextInt();
+		for (int x=1;x<=n ;x++)
+		{
+			int count=0;
+			for (int i=1;i<=x ;i++ )
+			{
+				if (x%i==0)
+				{
+					count++;
+				}
+			}
+			if (count==2)
+			{
+				System.out.println(x);
+			}
+		}
+	}
+}
