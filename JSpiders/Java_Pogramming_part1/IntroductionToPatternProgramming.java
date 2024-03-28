5783cbc90e88ba1e1a47a8a3a3c0033ad584c6c8
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-03-28 22:09:19 +0530

    Introduction To Pattern Programming

---
+++ JSpiders/Java_Pogramming_part1/IntroductionToPatternProgramming.java
@@ -0,0 +1,14 @@
+class IntroductionToPatternProgramming 
+{
+	public static void main(String[] args) 
+	{
+		for (int set=1;set<=3 ;set++ )
+		{
+			for (int situp=1;situp<=3 ;situp++ )
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
