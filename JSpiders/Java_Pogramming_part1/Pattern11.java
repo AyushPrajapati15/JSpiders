060f62eafdc5c7ec006f0a909d1521f7945866d4
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-04-02 22:37:33 +0530

    Pattern-11

---
+++ JSpiders/Java_Pogramming_part1/Pattern11.java
@@ -0,0 +1,21 @@
+class Pattern11 
+{
+	public static void main(String[] args) 
+	{
+		for (int i=5;i>=1 ;i-- )
+		{
+			for (int j=5;j>=1 ;j-- )
+			{
+				if (i==j||i+j==6)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
