c2b985b49de47a9ceaa5dd8883f0f83c81e2b040
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-06-10 23:35:11 +0530

    EvenElements

---
+++ JSpiders/Java_libraries/array_programs/EvenElements.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class EvenElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] a = new int[size];
+        for (int i = 0; i <= a.length - 1; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i <= a.length - 1; i++) {
+            if (a[i] % 2 == 0) {
+                System.out.println(a[i]);
+            }
+        }
+    }
+}
