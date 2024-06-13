173b35200c38d077ef2b7b73700b998167097381
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-06-13 12:02:06 +0530

    Char Index

---
+++ JSpiders/Java_Pogramming_part2/first_program/src/org/jsp/demo/CharIndex.java
@@ -0,0 +1,12 @@
+package org.jsp.demo;
+
+public class CharIndex {
+	public static void main(String[] args) {
+		char crr[]= {'a','e','i','o','u'};
+		for(int index=0;index<crr.length;index++)
+		{
+			System.out.println(index);
+		}
+	}
+
+}
