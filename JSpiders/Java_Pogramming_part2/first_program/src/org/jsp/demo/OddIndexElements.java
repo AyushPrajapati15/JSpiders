fb61bb8222abcba9f6a9c8f8c9ba2fa07e1ac89b
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-06-21 15:56:37 +0530

    Odd Index Elements

---
+++ JSpiders/Java_Pogramming_part2/first_program/src/org/jsp/demo/OddIndexElements.java
@@ -0,0 +1,15 @@
+package org.jsp.demo;
+
+public class OddIndexElements {
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5,6,7,8,9};
+		for(int i=0;i<a.length;i++)
+		{
+			if(i%2==1)
+			{
+				System.out.println(a[i]);
+			}
+		}
+	}
+
+}
