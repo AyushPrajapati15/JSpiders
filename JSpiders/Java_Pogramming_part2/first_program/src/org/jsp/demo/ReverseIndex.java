820b8e2afac5f697df0c4c92baefd6fd0c8c4f7a
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-06-23 22:59:48 +0530

    Reverse Index

---
+++ JSpiders/Java_Pogramming_part2/first_program/src/org/jsp/demo/ReverseIndex.java
@@ -0,0 +1,13 @@
+package org.jsp.demo;
+
+public class ReverseIndex {
+	public static void main(String[] args) {
+		char crr[]= {'a','e','i','o','u'};
+		for(int index=crr.length-1;index>=0;index--)
+		{
+			System.out.println(index);
+		}
+	
+	}
+
+}
