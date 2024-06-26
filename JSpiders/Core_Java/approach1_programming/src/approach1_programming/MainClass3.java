080ca6199a3ee23b5c4cab3aaaa86ded97c80a0a
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-06-26 22:08:50 +0530

    MainClass3

---
+++ JSpiders/Core_Java/approach1_programming/src/approach1_programming/MainClass3.java
@@ -0,0 +1,13 @@
+package approach1_programming;
+
+public class MainClass3 {
+
+	public static void main(String[] args) {
+		Train t1=new Train();
+		t1.arrivalPoint="Chennai";
+		System.out.println(t1.trainName);
+		
+
+	}
+
+}
