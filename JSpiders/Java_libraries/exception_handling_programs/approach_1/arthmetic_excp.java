3c05a9f5eb2336d09a5fe8a909576645a608e8eb
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-06-28 23:27:11 +0530

    Arthmetic Exception approach_1

---
+++ JSpiders/Java_libraries/exception_handling_programs/approach_1/arthmetic_excp.java
@@ -0,0 +1,11 @@
+public class arthmetic_excp {
+    public static void main(String[] args) {
+        System.out.println("Main Starts");
+        int a = 10;
+        System.err.println(a+5);
+        System.err.println(a/2);
+        System.err.println(a-3);
+        System.out.println(a*8);
+    }
+    
+}
