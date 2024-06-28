5c3a56cc67f07c382eef4c68a9abdcac9941aea3
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-06-28 23:27:35 +0530

    SIOBE Exception approach-1

---
+++ JSpiders/Java_libraries/exception_handling_programs/approach_1/SIOBE.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class SIOBE {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = "Java";
+        int index = sc.nextInt();
+        System.out.println(s.charAt(index));
+    }
+}
