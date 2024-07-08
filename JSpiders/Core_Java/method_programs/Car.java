207bd4c0c04826ace2e6785b27bc5db893202cc7
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-07-08 23:31:31 +0530

    Car

---
+++ JSpiders/Core_Java/method_programs/Car.java
@@ -0,0 +1,21 @@
+class Car 
+{
+	public static void accelerate()
+	{
+		System.out.prin
+			tln("Car accelerate");
+
+	}
+	public static void move()
+	{
+		System.out.println("Car move at 100km/h");
+
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Main Starts...");
+		accelerate();
+		move();
+		System.out.println("Main Ends...");
+	}
+}
