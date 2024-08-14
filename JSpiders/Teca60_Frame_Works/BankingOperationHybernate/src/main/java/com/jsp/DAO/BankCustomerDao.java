f5de190d69c7b07b0976942234a3d43ceef5d3e5
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-08-14 23:44:25 +0530

    Bank Customer Dao

---
+++ JSpiders/Teca60_Frame_Works/BankingOperationHybernate/src/main/java/com/jsp/DAO/BankCustomerDao.java
@@ -0,0 +1,10 @@
+package com.jsp.DAO;
+
+import com.jsp.entity.BankCustomer;
+
+public interface BankCustomerDao {
+	long insertBankCustomerObject(BankCustomer bankCustomer);
+	BankCustomer selectBankCustomerDetailsByUsingEmailIdAndPassword(String emailid, int password);
+	int updateCustomerAmount(int accountNUmber,double balance);
+	
+}
