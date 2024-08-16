8ec0d721370763a51f73e539d2fac5f30ac5a936
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-08-17 00:33:59 +0530

    removed an empty line

--- JSpiders/Teca60_Frame_Works/BankingOperationHybernate/src/main/java/com/jsp/DAO/BankCustomerDao.java
+++ JSpiders/Teca60_Frame_Works/BankingOperationHybernate/src/main/java/com/jsp/DAO/BankCustomerDao.java
@@ -5,6 +5,5 @@ import com.jsp.entity.BankCustomer;
 public interface BankCustomerDao {
 	long insertBankCustomerObject(BankCustomer bankCustomer);
 	BankCustomer selectBankCustomerDetailsByUsingEmailIdAndPassword(String emailid, int password);
-	int updateCustomerAmount(int accountNUmber,double balance);
-	
+	int updateCustomerAmount(int accountNUmber,double balance);	
 }
