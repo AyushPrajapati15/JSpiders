f90d049a6b2b1c7f223e30ee024edbfdb6ebc20c
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-08-30 21:57:14 +0530

    NonVeg

---
+++ JSpiders/Teca60_Frame_Works/AutoWiredConfiguration/src/main/java/com/jsp/model/NonVeg.java
@@ -0,0 +1,10 @@
+package com.jsp.model;
+
+public class NonVeg implements TajBanjara {
+
+	@Override
+	public String menu() {
+		return "*****MENU*****\n Chicken,\n Mutton,\n Fish,\n Prawns,\n Crab";
+	}
+
+}
