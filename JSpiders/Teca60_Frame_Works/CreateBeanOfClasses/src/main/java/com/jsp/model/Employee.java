7d4e93cc662b1f7f93e174096aa9df11f1961493
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-09-01 21:47:09 +0530

    Employee

---
+++ JSpiders/Teca60_Frame_Works/CreateBeanOfClasses/src/main/java/com/jsp/model/Employee.java
@@ -0,0 +1,52 @@
+package com.jsp.model;
+
+public class Employee {
+	
+	private int id;
+	private int	deptno;
+	private double sal;
+	private String name;
+
+	public Employee() {
+		System.out.println("Creating bean class");
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+
+	public double getSal() {
+		return sal;
+	}
+
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", deptno=" + deptno + ", sal=" + sal + ", name=" + name + "]";
+	}
+
+
+}
