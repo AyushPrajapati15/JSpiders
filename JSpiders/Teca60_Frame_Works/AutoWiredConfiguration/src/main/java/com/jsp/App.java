3fe0190a44909faffb2c45fb4024670b0dce4810
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-09-01 21:46:06 +0530

    App

---
+++ JSpiders/Teca60_Frame_Works/AutoWiredConfiguration/src/main/java/com/jsp/App.java
@@ -0,0 +1,25 @@
+package com.jsp;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.jsp.model.Customer;
+import com.jsp.model.TajBanjara;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+//        System.out.println( "Hello World!" );
+    	ApplicationContext context=new ClassPathXmlApplicationContext("com/jsp/file/tajbanjaraconfig.xml");
+//    	TajBanjara tajBanjara = context.getBean(TajBanjara.class);
+//    	System.out.println(tajBanjara.menu());
+    	Customer customer = context.getBean(Customer.class);
+    	customer.display();
+    	
+    }
+}
