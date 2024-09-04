df0ba7d039bd06cc3d9dd5014f631cae5136658e
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-09-04 22:55:31 +0530

    App

--- JSpiders/Teca60_Frame_Works/AutoWiredConfiguration/src/main/java/com/jsp/App.java
+++ JSpiders/Teca60_Frame_Works/AutoWiredConfiguration/src/main/java/com/jsp/App.java
@@ -10,16 +10,14 @@ import com.jsp.model.TajBanjara;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-//        System.out.println( "Hello World!" );
-    	ApplicationContext context=new ClassPathXmlApplicationContext("com/jsp/file/tajbanjaraconfig.xml");
-//    	TajBanjara tajBanjara = context.getBean(TajBanjara.class);
-//    	System.out.println(tajBanjara.menu());
-    	Customer customer = context.getBean(Customer.class);
-    	customer.display();
-    	
+public class App {
+    public static void main(String[] args) {
+        // System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/jsp/file/tajbanjaraconfig.xml");
+        // TajBanjara tajBanjara = context.getBean(TajBanjara.class);
+        // System.out.println(tajBanjara.menu());
+        Customer customer = context.getBean(Customer.class);
+        customer.display();
+
     }
 }
