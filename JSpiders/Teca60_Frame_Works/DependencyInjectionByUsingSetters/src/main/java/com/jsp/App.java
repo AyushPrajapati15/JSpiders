ef7afdb7fddf832ac66798f9aad9d9ab0281a1ee
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-09-22 22:58:47 +0530

    App

--- JSpiders/Teca60_Frame_Works/DependencyInjectionByUsingSetters/src/main/java/com/jsp/App.java
+++ JSpiders/Teca60_Frame_Works/DependencyInjectionByUsingSetters/src/main/java/com/jsp/App.java
@@ -10,17 +10,14 @@ import com.jsp.model.Vehicle;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	ApplicationContext context=new ClassPathXmlApplicationContext("com/jsp/file/vehicleconfig.xml");
-    	Vehicle vehicle = context.getBean(Vehicle.class);
-    	System.out.println(vehicle);
-    	
-//    	Engine engine = context.getBean(Engine.class);
-//    	System.out.println(engine);
-    	
-    	
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/jsp/file/vehicleconfig.xml");
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println(vehicle);
+
+        // Engine engine = context.getBean(Engine.class);
+        // System.out.println(engine);
+
     }
 }
