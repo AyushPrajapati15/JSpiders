eec04040c8bb4c5813326e9c3a8b36062adb14a7
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-10-07 21:25:22 +0530

    Hoisting js

---
+++ JSpiders/Web_tech/javascript/Day-4/Hoisting.js
@@ -0,0 +1,52 @@
+// console.log(a);
+// var a = 10;
+// console.log(a);
+
+// console.log(a);
+// let a = 10;
+
+
+// console.log(a);
+// const a = 10;
+
+
+
+// function num()
+// {
+//     var a = 10;
+//     b = 10;
+
+// }
+// num();
+// console.log(a);
+
+
+
+
+// var num = 10;
+// console.log(typeof num);
+// console.log(typeof "Ayush");
+// console.log(typeof 'A');
+// console.log(typeof true);
+// console.log(typeof false);
+// console.log(typeof null);
+// console.log(null);
+// console.log(typeof typeof num);
+
+
+// var num = 12365468497465125468965132549;
+// var num2 = BigInt(12365468497465125468965132549);
+// console.log(num);
+// console.log(num2, typeof num2);
+
+
+// var sym = Symbol();
+// console.log(sym, typeof sym);
+
+// var sym1 = Symbol();
+// console.log(sym == sym1);
+
+
+
+var num = NaN;
+console.log(typeof num);
