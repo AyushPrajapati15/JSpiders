8d7c578bbb5c0b29914e468b99e2e81a28d5190b
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-10-09 23:22:58 +0530

    App

--- JSpiders/Teca60_Frame_Works/DependencyInjectionByUsingConstructor/src/main/java/com/jsp/App.java
+++ JSpiders/Teca60_Frame_Works/DependencyInjectionByUsingConstructor/src/main/java/com/jsp/App.java
@@ -10,16 +10,14 @@ import com.jsp.model.Employee;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-//        System.out.println( "Hello World!" );
-    	ApplicationContext context=new ClassPathXmlApplicationContext("com/jsp/file/employee.xml");
-    	Employee employee = context.getBean("employee",Employee.class);
-    	System.out.println(employee);
-    	
-//    	Address address = context.getBean("address1",Address.class);
-//    	System.out.println(address);
+public class App {
+    public static void main(String[] args) {
+        // System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/jsp/file/employee.xml");
+        Employee employee = context.getBean("employee", Employee.class);
+        System.out.println(employee);
+
+        // Address address = context.getBean("address1",Address.class);
+        // System.out.println(address);
     }
 }
