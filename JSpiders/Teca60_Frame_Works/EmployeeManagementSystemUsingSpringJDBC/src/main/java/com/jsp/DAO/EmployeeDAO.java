9b145e111d55983476f70bf5d4a2910037d52fbc
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-11-16 22:18:07 +0530

    Employee DAO

---
+++ JSpiders/Teca60_Frame_Works/EmployeeManagementSystemUsingSpringJDBC/src/main/java/com/jsp/DAO/EmployeeDAO.java
@@ -0,0 +1,11 @@
+package com.jsp.DAO;
+
+public interface EmployeeDAO {
+	void insertEmployeeDetails();
+	void updateEmployeeDetailsByDptNo();
+	void updateDeptNoByEmployeeId();
+	void selectEmployeeById();
+	void selectyAllEmployee();
+
+}
+
