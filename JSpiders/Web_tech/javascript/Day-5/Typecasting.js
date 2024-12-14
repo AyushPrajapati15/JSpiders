72154cb70ade715ae386adb7d1ee61a7f6d09ac3
AyushPrajapati15 <aayushprajapati867@gmail.com>
2024-12-14 21:40:26 +0530

    Typecasting js

---
+++ JSpiders/Web_tech/javascript/Day-5/Typecasting.js
@@ -0,0 +1,71 @@
+// var a = 10;
+// console.log(typeof (a), a);
+
+// var b = "10";
+// console.log(typeof(b),b)
+
+// var a = a + b;
+// console.log(typeof (a), a);
+// var a = a * b;
+
+// console.log(typeof (a), a);
+
+// console.log(typeof (a == b), a == b);
+
+
+
+//NUMBER TYPE
+// var a = "10.5a";
+// console.log(typeof a,a);
+
+// var a=Number(a)
+// console.log(typeof(a),a)
+
+//INT TYPE
+
+// var a = parseInt(a);
+// console.log(typeof a,a)
+
+//FLOAT TYPE
+
+// var num = parseFloat(a);
+// console.log(typeof num,num);
+
+
+
+//STRING TYPE
+
+// var num = 10;
+// console.log(typeof num, num);
+// var str = String(num);
+// var str = num.toString();
+// console.log(typeof str, str);
+
+
+//BOOLEAN TYPE
+// var bool = false;
+// console.log(Number(bool));
+
+// var vari = true;
+// var bool = Boolean(vari);
+// console.log(typeof bool,bool);
+// var vari = NaN;
+// var bool = Boolean(vari);
+// console.log(typeof bool,bool);
+// var vari = 10;
+// var bool = Boolean(vari);
+// console.log(typeof bool,bool);
+// var vari = 0;
+// var bool = Boolean(vari);
+// console.log(typeof bool,bool);
+// var vari = 0.000001;
+// var bool = Boolean(vari);
+// console.log(typeof bool,bool);
+// var vari = "";
+// var bool = Boolean(vari);
+// console.log(typeof bool,bool);
+// var vari = "hi";
+// var bool = Boolean(vari);
+// console.log(typeof bool,bool);
+
+
