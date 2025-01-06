05808e06120519ee1c65a9cead8647c1cda04556
AyushPrajapati15 <aayushprajapati867@gmail.com>
2025-01-06 22:36:28 +0530

    Anchor js

---
+++ JSpiders/Web_tech/javascript/Day-18/anchor.js
@@ -0,0 +1,12 @@
+// var allAnchor=document.anchors
+
+// allAnchor[0].innerHTML="FaceBook"
+// allAnchor[0].href="https://www.facebook.com"
+// allAnchor[0].target="_blank"
+
+
+// allAnchor[1].innerHTML="Instagram"
+// allAnchor[1].href="https://www.instagram.com"
+
+document.title="Facebook"
+//document.title and document.domin
