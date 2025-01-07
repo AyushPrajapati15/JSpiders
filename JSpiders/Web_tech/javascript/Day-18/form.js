6138f7bf705f0203bacba009fa29accb84fab1c6
AyushPrajapati15 <aayushprajapati867@gmail.com>
2025-01-07 22:19:07 +0530

    Form Js

---
+++ JSpiders/Web_tech/javascript/Day-18/form.js
@@ -0,0 +1,12 @@
+var allforms=document.forms;
+allforms[0].elements[0].type="radio"
+allforms[0].elements[1].type="checkbox"
+allforms[0].elements[2].type="color"
+
+allforms[1].elements[0].type="email"
+allforms[1].elements[0].placeholder="Enter the Email"
+allforms[1].elements[1].type="password"
+allforms[1].elements[1].placeholder="Enter the Password"
+
+allforms[1].elements[2].type="submit"
+allforms[1].action="https://www.tesla.com"
