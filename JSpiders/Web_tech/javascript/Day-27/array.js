c94c569a94eb72a698fbcf93b972d0021ca3653b
AyushPrajapati15 <aayushprajapati867@gmail.com>
2025-04-10 21:03:36 +0530

    array

---
+++ JSpiders/Web_tech/javascript/Day-27/array.js
@@ -0,0 +1,59 @@
+// var arr = ["ayush", "MS015", "Male", 85000, true];
+// console.log(arr);
+
+
+// for (i = 0; i < 5; i++) {
+//     console.log(arr[i]);
+    
+    
+// }
+// arr[0] = "ayu";
+
+// console.log(arr);
+
+
+// var arr = [];
+// arr[0] = "Apple";
+// arr[2] = "Mango";
+// console.log(arr);
+
+
+//BY USING ARRAY OBJECT
+
+// var arr = new Array("red", "grees", "blue");
+// console.log(arr);
+
+// ************************************************************SPLIT***************************************************
+// var arr = "Apple mango banana graper";
+// console.log(arr.split(" "), typeof (arr))
+
+// ************************************************************JOIN***************************************************
+
+// var arr= ['Apple', 'mango', 'banana', 'grapes']
+// console.log(arr);
+// var str = arr.join("");
+// console.log(str);
+
+
+
+// var arr = ['Apple', 'mango', 'banana', 'grapes']
+// console.log(arr);
+
+// var str = arr.toString();
+// console.log(str);
+// var str1=str.replaceAll("," ," ")
+// console.log(str1);
+// var arrRev = arr.reverse();
+// console.log(arrRev);
+
+
+
+var arr = ['Apple', 'mango', 'banana', 'grapes']
+arr.pop();
+arr.push("white","black");
+console.log(arr);
+
+var arr = ['Apple', 'mango', 'banana', 'grapes']
+arr.shift();
+arr.unshift("white", "black");
+console.log(arr);
