5086888e128788686208611fa920c2b6ad60b93d
AyushPrajapati15 <aayushprajapati867@gmail.com>
2025-04-15 23:42:29 +0530

    assignrev

---
+++ JSpiders/Web_tech/javascript/Day-27/assignrev.js
@@ -0,0 +1,15 @@
+var str = `she shells seashells by the seashore,
+The shells she sells are surely seashells
+So if she sells shells on the seashore
+I'm sure she sells seashore shells.`;
+
+var str1 = str.split("");
+console.log(str1);
+
+var strrev = str1.reverse();
+console.log(strrev);
+
+var join=strrev.join("")
+console.log(join);
+
+
